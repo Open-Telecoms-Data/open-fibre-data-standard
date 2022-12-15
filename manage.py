62dfe0517f3caec509709a71af03f72bd57ed28e
Duncan Dewhurst <duncan.dewhurst@opendataservices.coop>
2022-12-15 16:32:53 +1300

    manage.py: Remove update_from_airtable

--- manage.py
+++ manage.py
@@ -14,25 +14,14 @@ from flattentool import create_template, flatten
 from github import Github
 from ocdskit.mapping_sheet import mapping_sheet
 from pathlib import Path
-from pyairtable import Table
-from pyairtable.formulas import match
 
-AIRTABLE_API_KEY = os.environ['AIRTABLE_API_KEY']
-BASE_ID = 'apprMa4GXD05csfkW'
 GITHUB_ACCESS_TOKEN = os.environ['GITHUB_ACCESS_TOKEN']
-EXTERNAL_CODELISTS = ['country.csv', 'currency.csv', 'language.csv', 'mediaType.csv']
 
 basedir = Path(__file__).resolve().parent
 codelistdir = basedir / 'codelists'
 examplesdir = basedir / 'examples'
 referencedir = basedir / 'docs' / 'reference'
 schemadir = basedir / 'schema'
-
-def set_value(source_obj, target_obj, source_field, target_field):
-    """Update the value of the target object's field if the equivalent field exists in the source object."""
-    
-    if source_obj.get(source_field):
-        target_obj[target_field] = source_obj[source_field]
 
 
 def read_lines(filename):
@@ -486,181 +475,6 @@ def update_schema_docs(schema):
 @click.group()
 def cli():
     pass
-
-
-@cli.command()
-def update_from_airtable():
-    """Update schema and codelists from Airtable.
-    
-    * Add new definitions and properties
-    * Update existing definitions and properties
-    * Delete definitions and properties that do not exist in Airtable or are marked as 'Omit' 
-    """
-
-    top_object = 'Network'
-    
-    with (schemadir / 'network-schema.json').open() as f:
-        schema = json.load(f)
-    
-    # Get data from Airtable
-    definitions_table = Table(AIRTABLE_API_KEY, BASE_ID, 'Classes')
-    properties_table = Table(AIRTABLE_API_KEY, BASE_ID, 'Properties')
-    codelists_table = Table(AIRTABLE_API_KEY, BASE_ID, 'Codelists')
-    codes_table = Table(AIRTABLE_API_KEY, BASE_ID, 'Codes')  
-
-    # Update schema
-    definitions = {record['id']: record for record in definitions_table.all()}
-    for definition_record in definitions.values():
-        definition_fields = definition_record['fields']
-        
-        if definition_fields['Status'] != 'Omit':
-            if definition_fields['Name'] == top_object:
-                target = schema
-            else:
-                if definition_fields['Name'] not in schema['definitions']:
-                    target = schema['definitions'][definition_fields['Name']] = {}
-                else:
-                    target = schema['definitions'][definition_fields['Name']]
-
-            set_value(definition_fields, target, "Title", "title")
-            set_value(definition_fields, target, "Description", "description")
-            set_value(definition_fields, target, "Status", "$comment")
-            if "additionalFields" in definition_fields:
-                target["additionalFields"] = bool(definition_fields["additionalFields"])
-            
-            target["type"] = "object"
-            
-            # Add links to related Github issues
-            target['$comment'] = ",".join(definition_fields.get("Github issues",""))
-
-            # Update properties
-            if not target.get("properties"):
-                target["properties"] = {}
-            
-            # Clear required fields
-            if target.get("required"):
-                target["required"] = []
-
-            if definition_fields.get('Properties'):
-                for property_id in definition_fields['Properties']:
-                    property_record = properties_table.get(property_id)
-                    property_fields = property_record['fields']
-                    
-                    if property_fields['Status'] != 'Omit':
-                        
-                        # Add new properties or update existing properties
-                        if property_fields['Property'] not in target["properties"]:
-                            property = target["properties"][property_fields['Property']] = {}
-                        else:
-                            property = target["properties"][property_fields['Property']]
-        
-                        # Set values
-                        set_value(property_fields, property, "Title", "title")
-                        set_value(property_fields, property, "Description", "description")
-                        set_value(property_fields, property, "Github issues", "$comment")
-                        set_value(property_fields, property, "Format", "format")
-                        set_value(property_fields, property, "Type", "type")
-                        set_value(property_fields, property, "Constant value", "const")
-
-                        # Add links to related Github issues
-                        property['$comment'] = ",".join(property_fields.get("Github issues",""))
-
-                        # Set $ref for objects and arrays of objects
-                        if 'instanceOf' in property_fields:
-                            ref = definitions[property_fields['instanceOf'][0]]['fields']['Name']
-
-                        if property_fields.get('Type') == 'object':
-                            property.pop("type")
-                            property['$ref'] = f'#/definitions/{ref}'
-                        elif property_fields.get('Type') == 'array':
-                            if property_fields.get("Items"):
-                                property["items"] = {"type": property_fields["Items"]}
-                                if property_fields.get("instanceOf"):
-                                    property["items"] = {"$ref": f'#/definitions/{ref}'}
-                        
-                        # Set codelist, enum and openCodelist
-                        if property_fields.get("Codelist"):
-                            codelist_record = codelists_table.get(property_fields["Codelist"][0])
-                            codelist_fields = codelist_record["fields"]
-                            property["codelist"] = f"{codelist_fields['Name']}.csv"
-                            
-                            if codelist_fields.get("Codelist type") == "Closed":
-                                property["openCodelist"] = False
-                                property["enum"] = []
-                                
-                                if codelist_fields.get("Codes"):
-                                    for code_id in codelist_fields["Codes"]:
-                                        code_record = codes_table.get(code_id)
-                                        property["enum"].append(code_record["fields"]["Code"])
-                            
-                            elif codelist_fields.get("Codelist type") == "Open":
-                                property["openCodelist"] = True
-
-                        # Set required properties
-                        if property_fields.get("Required") == True:
-                            if target.get("required"):
-                                if property_fields["Property"] not in target["required"]:
-                                    target["required"].append(property_fields["Property"])
-                            else:
-                                target["required"] = [property_fields["Property"]]
-
-                        target["properties"][property_fields['Property']] = property
-            
-            # Delete properties not in Airtable
-            for property in list(target["properties"]):
-                formula = match({"Property": property, "propertyOf": definition_fields["Name"]})
-                result = properties_table.first(formula=formula)
-
-                if not result or result["fields"]["Status"] == "Omit":
-                    target["properties"].pop(property)
-
-    # Delete definitions not in Airtable
-    for definition in list(schema["definitions"]):
-        formula = match({"Name": definition})
-        result = definitions_table.first(formula=formula)
-
-        if not result or result["fields"]["Status"] == "Omit":
-            schema["definitions"].pop(definition)
-
-    with (schemadir / 'network-schema.json').open('w') as f:
-        json.dump(schema, f, indent=2)
-        f.write('\n')
-    
-    # Update codelists
-    files = glob.glob(f"{codelistdir}/*/*.csv")
-    for file in files:
-      
-      # Don't delete codelists that are managed outside of Airtable
-      if file.split("/")[-1] not in EXTERNAL_CODELISTS:
-        os.remove(file)
-    
-    for codelist in codelists_table.all():
-        codelist_fields = codelist["fields"]
-        
-        if codelist_fields["Status"] != "Omit" and codelist_fields.get("Codes"):
-            filename = f"{codelist_fields['Name']}.csv"
-            
-            if codelist_fields.get("Codelist type") == "Closed":
-                subdir = "closed"
-            else:
-                subdir = "open"
-
-            with open(codelistdir / subdir / filename, 'w', newline='') as f:
-                fieldnames = ['Code', 'Title', 'Description']
-                writer = csv.DictWriter(f, fieldnames=fieldnames)
-                writer.writeheader()
-
-                for code_id in codelist_fields['Codes']:
-                    code_record = codes_table.get(code_id)
-                    code_fields = code_record['fields']
-
-                    if code_fields["Status"] not in ["Omit","Further clarification needed"]:
-
-                        writer.writerow({
-                            'code': code_fields.get("Code", ""),
-                            'title': code_fields.get("Title", ""),
-                            'description': code_fields.get("Description", "")
-                        })
 
 
 @cli.command()
