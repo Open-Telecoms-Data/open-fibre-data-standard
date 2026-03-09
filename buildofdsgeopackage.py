4fe0641475672325d9f5927c8bf52333bfa1da09
Bee Webb <bee.webb@opendataservices.coop>
2026-03-09 11:40:40 +0000

    buildofdsgeopackage: Lint with black

--- buildofdsgeopackage.py
+++ buildofdsgeopackage.py
@@ -9,16 +9,20 @@ from referencing.jsonschema import DRAFT202012
 
 MAP_FIELD_TYPES_TO_SQLITE_TYPES = {"boolean": "int", "integer": "int", "number": "real"}
 
+
 def deref(obj, registry):
-    if isinstance(obj, list): return [deref(i, registry) for i in obj]
-    if not isinstance(obj, dict): return obj
-    
+    if isinstance(obj, list):
+        return [deref(i, registry) for i in obj]
+    if not isinstance(obj, dict):
+        return obj
+
     # Merge $ref contents with local keys (2020-12 style)
     if "$ref" in obj:
         resolved = registry.resolver().lookup(obj.pop("$ref")).contents
         obj = {**resolved, **obj}
-        
+
     return {k: deref(v, registry) for k, v in obj.items()}
+
 
 class Builder:
 
@@ -35,13 +39,13 @@ class Builder:
             "Start": "nodes",
             "End": "nodes",
             "Wayleaves": "wayleaves",
-            "Grantor": "organisations"
+            "Grantor": "organisations",
         }
         self.MAPPING_MANY_TO_MANY_KEY_NAMES_TO_LAYERS = {
             "Network providers": "organisations",
             "Funders": "organisations",
             "Related phases": "phases",
-            "Wayleaves": "wayleaves"
+            "Wayleaves": "wayleaves",
         }
 
     def _load_codelist_items(
@@ -160,9 +164,7 @@ class Builder:
                             description
                         )
                         VALUES (?, ?);
-                        """.format(
-                            table_name
-                        ),
+                        """.format(table_name),
                         [code, desc],
                     )
 
@@ -269,15 +271,11 @@ class Builder:
         )
 
         if geographic_type:
-            self.cursor.execute(
-                """
+            self.cursor.execute("""
                 INSERT INTO gpkg_geometry_columns (
                     table_name, column_name, geometry_type_name, srs_id, z, m
                 ) VALUES ('{}', 'geom', '{}', 4326, 0, 0);
-            """.format(
-                    table_name, geographic_type
-                )
-            )
+            """.format(table_name, geographic_type))
 
         self.information_out["tables"][table_name] = {
             "columns": columns,
@@ -586,10 +584,12 @@ class Builder:
         with open(jsonschema_filename) as fp:
             jsonschema = json.load(fp)
 
-        reg = Registry().with_resource(uri="", resource=DRAFT202012.create_resource(jsonschema))
+        reg = Registry().with_resource(
+            uri="", resource=DRAFT202012.create_resource(jsonschema)
+        )
 
         jsonschema = deref(jsonschema, reg)
-        
+
         # Copy GeoPackage
         sqlite_filename = os.path.join(
             self.root_directory,
@@ -613,34 +613,28 @@ class Builder:
             "closed_codelists": {},
         }
         # Create extension tables
-        self.cursor.execute(
-            """
+        self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS gpkgext_relations (
                 id INTEGER PRIMARY KEY AUTOINCREMENT, base_table_name TEXT NOT NULL, base_primary_column TEXT NOT NULL, related_table_name TEXT NOT NULL, related_primary_column TEXT NOT NULL, relation_type TEXT NOT NULL, mapping_table_name TEXT UNIQUE
             );
-        """
-        )
+        """)
         # Create gpkg_data_columns per https://www.geopackage.org/spec120/#gpkg_data_columns_sql
         # EXCEPT don't make the name column UNIQUE, this causes us clashes
         # https://www.geopackage.org/spec120/#gpkg_data_columns_cols says "A human-readable identifier (e.g. short name) for the column_name content" so why unique?
         # And indeed this is changed in the unreleased version https://www.geopackage.org/spec/#gpkg_data_columns_sql
-        self.cursor.execute(
-            """
+        self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS gpkg_data_columns (
                 table_name TEXT NOT NULL, column_name TEXT NOT NULL, name TEXT, title TEXT, description TEXT, mime_type TEXT, constraint_name TEXT,
                 CONSTRAINT pk_gdc PRIMARY KEY (table_name, column_name),
                 CONSTRAINT fk_gdc_tn FOREIGN KEY (table_name) REFERENCES gpkg_contents(table_name)
             );
-        """
-        )
+        """)
         # Create gpkg_data_column_constraints per https://www.geopackage.org/spec120/#gpkg_data_column_constraints_sql
-        self.cursor.execute(
-            """
+        self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS gpkg_data_column_constraints (
                 constraint_name TEXT NOT NULL, constraint_type TEXT NOT NULL, value TEXT, min NUMERIC, min_is_inclusive BOOLEAN, max NUMERIC, max_is_inclusive BOOLEAN, description TEXT, CONSTRAINT gdcc_ntv UNIQUE (constraint_name, constraint_type, value)
             );
-        """
-        )
+        """)
         # Register extensions
         self.cursor.execute(
             "INSERT OR IGNORE INTO gpkg_extensions (table_name, column_name, extension_name, definition, scope) VALUES ('gpkgext_relations', NULL, 'gpkg_related_tables','http://docs.opengeospatial.org/is/18-000/18-000.html', 'read-write');"
